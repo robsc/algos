0479964883d46f1d7091633e814dc51e81c5ea7d
Robert Schonberger <rschonberger@twitter.com>
2012-07-29 14:04:26 -0400

    use expect instead of the lame assert

--- src/test/scala/com/rschonberger/algorithm/StableMarriageTest.scala
+++ src/test/scala/com/rschonberger/algorithm/StableMarriageTest.scala
@@ -4,7 +4,6 @@ import org.scalatest._
 
 class StableMarriageSuite extends FunSuite {
   test("The x is cheese") {
-    val myCheese: String = "Cheese"
-    assert (myCheese == StableMarriage.x)
+    expect("Cheese")(StableMarriage.x)
   }
 }

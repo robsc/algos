e297ec42754e9f27b8454adfa24e3c9bd8e3fb58
Robert Schonberger <rschonberger@twitter.com>
2012-07-29 22:01:48 -0400

    add a two person example

--- src/test/scala/com/rschonberger/algorithm/StableMarriageTest.scala
+++ src/test/scala/com/rschonberger/algorithm/StableMarriageTest.scala
@@ -8,6 +8,22 @@ class StableMarriageSuite extends FunSuite {
     expect("Cheese")(StableMarriage.x)
   }
 
+  test("Two Person Example") {
+    val men: Seq[String] = "a" :: "b" :: List.empty
+    val women: Set[Int] = HashSet.empty + 1 + 2
+    val expectedResult = Map.empty + (1 -> "b") + (2 -> "a")
+    val score = (a: String, b: Int) => {
+      (a, b) match {
+        case ("a", 2) => 2
+        case ("b", 1) => 2
+        case _ => 1
+      }
+    }
+    val actualResult = StableMarriage.finalMarriages(men, women, score)
+    expect(expectedResult)(actualResult)
+  }
+
+
   test("One Person example") {
     val men: Seq[String] =  "a" :: List.empty
     val women: Set[Int] = HashSet.empty + 1
